sbahling/snd-firewire-improve

commit 231bd17e665bb83f292d4077b57735540ec04501

firewire-tascam: fix to use memory object in GFP_KERNEL instead of vmalloc
An memory object allocated in kernel virtual address can brings page
fault. This means that accesses to the area should not be done in
any IRQ context. However, packet handler run in tasklet.

This commit uses GFP_KERNEL instead of vmalloc to prevent unexpected
page fault.

sound/firewire/tascam/tascam-hwdep.c
@@ -180,7 +180,7 @@ static int __maybe_unused hwdep_vm_fault(struct vm_fault *vmf)
 		return VM_FAULT_SIGBUS;
 	addr += offset;
 
-	page = vmalloc_to_page(addr);
+	page = virt_to_page(addr);
 	get_page(page);
 	vmf->page = page;
 
@@ -220,7 +220,7 @@ static void hwdep_free(struct snd_hwdep *hwdep)
 {
 	struct snd_tscm *tscm = hwdep->private_data;
 
-	vfree(tscm->status);
+	kfree(tscm->status);
 	tscm->status = NULL;
 }
 
@@ -237,7 +237,7 @@ int snd_tscm_create_hwdep_device(struct snd_tscm *tscm)
 	struct snd_hwdep *hwdep;
 	int err;
 
-	tscm->status = vzalloc(sizeof(*tscm->status));
+	tscm->status = kzalloc(sizeof(*tscm->status), GFP_KERNEL);
 	if (!tscm->status)
 		return -ENOMEM;
 

commit 5e7fef9e48fea9630bd8d4602727e13a566000c9

firewire-tascam: use one page for mmap(2)ed data
When using kernel logical address space instead of kernel virtual space
I should have keep one page because a call of kmalloc() keeps memory
object from in-kernel SLAB/SLOB/SLUB allocators and the object is not
aligned to page boundary.

Fixes: 231bd17e665b('firewire-tascam: fix to use memory object in GFP_KERNEL instead of vmalloc')

sound/firewire/tascam/tascam-hwdep.c
@@ -218,7 +218,7 @@ static void hwdep_free(struct snd_hwdep *hwdep)
 {
 	struct snd_tscm *tscm = hwdep->private_data;
 
-	kfree(tscm->status);
+	snd_free_pages((void *)tscm->status, PAGE_ALIGN(sizeof(*tscm->status)));
 	tscm->status = NULL;
 }
 
@@ -235,13 +235,15 @@ int snd_tscm_create_hwdep_device(struct snd_tscm *tscm)
 	struct snd_hwdep *hwdep;
 	int err;
 
-	tscm->status = kzalloc(sizeof(*tscm->status), GFP_KERNEL);
+	tscm->status = snd_malloc_pages(PAGE_ALIGN(sizeof(*tscm->status)),
+					GFP_KERNEL);
 	if (!tscm->status)
 		return -ENOMEM;
 
 	err = snd_hwdep_new(tscm->card, "Tascam", 0, &hwdep);
 	if (err < 0) {
-		kfree(tscm->status);
+		snd_free_pages((void *)tscm->status,
+			       PAGE_ALIGN(sizeof(*tscm->status)));
 		tscm->status = NULL;
 		return err;
 	}