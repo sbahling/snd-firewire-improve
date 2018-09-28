sbahling/snd-firewire-improve

commit a384019c0f7858b013261e67d2c462c1522281b3

firewire-tascam: add MMAP support to show status and control message
TASCAM FireWire series transfers status and control messages in
isochronous packets. One packet includes one message, and the
message is periodic every 64 packets. As a result, the messages
construct image with 64 quadlets. For example:

00 00000000, 32 00000000
01 00000000, 33 00000000
02 00000000, 34 00000000
03 00020000, 35 00000000
04 00000000, 36 25ea0000
05 ffff0000, 37 27dfff00
06 ffffffff, 38 1d9d0000
07 ffffffff, 39 00000000
08 ffffffff, 40 25ea0000
09 ffffffff, 41 27dfff00
10 00000000, 42 00000000
11 00000000, 43 00000000
12 00000000, 44 00000000
13 00000000, 45 00000000
14 00000000, 46 00000000
15 00010000, 47 00000000
16 00012e00, 48 00000000
17 00010400, 49 00000000
18 00011e00, 50 00000000
19 00011200, 51 00000000
20 00014900, 52 01010101
21 00011e00, 53 00000000
22 00010c00, 54 25ea0000
23 00013000, 55 27dfff00
24 00000000, 56 00000000
25 00000000, 57 00000000
26 00000000, 58 00000000
27 00000000, 59 00000001
28 00000000, 60 00000000
29 00000000, 61 00000000
30 00000000, 62 00000000
31 00000000, 63 00000000

Quadlet 00-15 show control messages. Quadlet 16-23 show analog input
level. Quadlet 24-31 shows digital ADAT input level. Quadlet 32-33
shows digital S/PDIF input level. Quadlet 34-35 is unknown. Quadlet
36-43 shows analog output level. The other quadlets are unknown.

This image is updated every 1 msec or less, depending on current
sampling transfer frequency.

This commit adds MMAP support to show this image via hwdep interface.
An userspace application can map a page frame to its virtual address
space with read-only flag. This driver write every control and status
messages into the page frame, and the userspace application can parse
them.

The control messages are both of edge-triggered/level-trigerred,
according to physical implementation. For example, the control message
corresponding to toggle switch acts as edge-trigger, while the control

sound/firewire/tascam/tascam-hwdep.c
@@ -132,6 +132,8 @@ static int hwdep_release(struct snd_hwdep *hwdep, struct file *file)
 		tscm->dev_lock_count = 0;
 	spin_unlock_irq(&tscm->lock);
 
+	vfree(tscm->status);
+
 	return 0;
 }
 
@@ -163,6 +165,65 @@ static int hwdep_compat_ioctl(struct snd_hwdep *hwdep, struct file *file,
 #define hwdep_compat_ioctl NULL
 #endif
 
+static int __maybe_unused hwdep_vm_fault(struct vm_fault *vmf)
+{
+	struct snd_tscm *tscm = vmf->vma->vm_private_data;
+	u8 *addr = (u8 *)tscm->status;
+	unsigned long offset;
+	struct page *page;
+
+	if (!tscm)
+		return VM_FAULT_SIGBUS;
+
+	offset = vmf->pgoff << PAGE_SHIFT;
+	if (offset < PAGE_ALIGN(sizeof(tscm->status)) - PAGE_SIZE)
+		return VM_FAULT_SIGBUS;
+	addr += offset;
+
+	page = vmalloc_to_page(addr);
+	get_page(page);
+	vmf->page = page;
+
+	return 0;
+}
+
+static const __maybe_unused struct vm_operations_struct hwdep_vm_ops = {
+	.fault	= hwdep_vm_fault,
+};
+
+/* Supported only on cache coherent architectures. */
+#if defined(CONFIG_X86) || defined(CONFIG_PPC) || defined(CONFIG_ALPHA)
+static int __maybe_unused hwdep_mmap(struct snd_hwdep *hwdep,
+				struct file *filp, struct vm_area_struct *vma)
+{
+	struct snd_tscm *tscm = hwdep->private_data;
+	unsigned long requested_pages, actual_pages;
+
+	if (!(vma->vm_flags & VM_READ))
+		return -EINVAL;
+
+	requested_pages = vma_pages(vma);
+	actual_pages = (((u64)tscm->status & ~PAGE_MASK) +
+			sizeof(*tscm->status)+ PAGE_SIZE - 1) >> PAGE_SHIFT;
+	if (requested_pages < actual_pages)
+		return -EINVAL;
+
+	vma->vm_ops = &hwdep_vm_ops;
+	vma->vm_flags |= VM_DONTEXPAND | VM_DONTDUMP;
+	vma->vm_private_data = tscm;
+
+	return 0;
+}
+#endif
+
+static void hwdep_free(struct snd_hwdep *hwdep)
+{
+	struct snd_tscm *tscm = hwdep->private_data;
+
+	vfree(tscm->status);
+	tscm->status = NULL;
+}
+
 int snd_tscm_create_hwdep_device(struct snd_tscm *tscm)
 {
 	static const struct snd_hwdep_ops ops = {
@@ -171,18 +232,27 @@ int snd_tscm_create_hwdep_device(struct snd_tscm *tscm)
 		.poll		= hwdep_poll,
 		.ioctl		= hwdep_ioctl,
 		.ioctl_compat	= hwdep_compat_ioctl,
+		.mmap		= hwdep_mmap,
 	};
 	struct snd_hwdep *hwdep;
 	int err;
 
+	tscm->status = vzalloc(sizeof(*tscm->status));
+	if (!tscm->status)
+		return -ENOMEM;
+
 	err = snd_hwdep_new(tscm->card, "Tascam", 0, &hwdep);
-	if (err < 0)
+	if (err < 0) {
+		vfree(tscm->status);
+		tscm->status = NULL;
 		return err;
+	}
 
 	strcpy(hwdep->name, "Tascam");
 	hwdep->iface = SNDRV_HWDEP_IFACE_FW_TASCAM;
 	hwdep->ops = ops;
 	hwdep->private_data = tscm;
+	hwdep->private_free = hwdep_free;
 	hwdep->exclusive = true;
 
 	return err;

include/uapi/sound/firewire.h
@@ -88,4 +88,8 @@ struct snd_firewire_get_info {
  * Returns -EBUSY if the driver is already streaming.
  */
 
+struct snd_firewire_tascam_status {
+	__u32 status[64];
+};
+
 #endif /* _UAPI_SOUND_FIREWIRE_H_INCLUDED */

sound/firewire/tascam/amdtp-tascam.c
@@ -117,6 +117,21 @@ int amdtp_tscm_add_pcm_hw_constraints(struct amdtp_stream *s,
 	return amdtp_stream_add_pcm_hw_constraints(s, runtime);
 }
 
+static void read_control_messages(struct amdtp_stream *s,
+				  __be32 *buffer, unsigned int data_blocks)
+{
+	struct snd_tscm *tscm = container_of(s, struct snd_tscm, tx_stream);
+	unsigned int index = 0;
+	unsigned int i;
+
+	for (i = 0; i < data_blocks; i++) {
+		index = be32_to_cpu(buffer[0]) % 64;
+		tscm->status->status[index] =
+				be32_to_cpu(buffer[s->data_block_quadlets - 1]);
+		buffer += s->data_block_quadlets;
+	}
+}
+
 static unsigned int process_tx_data_blocks(struct amdtp_stream *s,
 					   __be32 *buffer,
 					   unsigned int data_blocks,
@@ -128,7 +143,7 @@ static unsigned int process_tx_data_blocks(struct amdtp_stream *s,
 	if (data_blocks > 0 && pcm)
 		read_pcm_s32(s, pcm, buffer, data_blocks);
 
-	/* A place holder for control messages. */
+	read_control_messages(s, buffer, data_blocks);
 
 	return data_blocks;
 }

sound/firewire/tascam/tascam.h
@@ -91,6 +91,9 @@ struct snd_tscm {
 
 	/* For MIDI message outgoing transactions. */
 	struct snd_fw_async_midi_port out_ports[TSCM_MIDI_OUT_PORT_MAX];
+
+	/* For control messages. */
+	struct snd_firewire_tascam_status *status;
 };
 
 #define TSCM_ADDR_BASE			0xffff00000000ull

commit e11d941f733c319a27933cbfe423b42eee03f8e0

firewire-tascam: fix double free corruption
A memocy object is released two times; by close(2) operation to
ALSA hwdep character device, by handler of card disconnection.

Fixes: a384019c0f78('firewire-tascam: add MMAP support to show status and control message')

sound/firewire/tascam/tascam-hwdep.c
@@ -132,8 +132,6 @@ static int hwdep_release(struct snd_hwdep *hwdep, struct file *file)
 		tscm->dev_lock_count = 0;
 	spin_unlock_irq(&tscm->lock);
 
-	kfree(tscm->status);
-
 	return 0;
 }
 