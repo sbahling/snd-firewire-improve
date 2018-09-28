sbahling/snd-firewire-improve

commit 231bd17e665bb83f292d4077b57735540ec04501

firewire-tascam: fix to use memory object in GFP_KERNEL instead of vmalloc
An memory object allocated in kernel virtual address can brings page
fault. This means that accesses to the area should not be done in
any IRQ context. However, packet handler run in tasklet.

This commit uses GFP_KERNEL instead of vmalloc to prevent unexpected
page fault.

sound/firewire/tascam/tascam-hwdep.c
@@ -180,7 +180,7 @@ static int __maybe_unused hwdep_vm_fault(struct vm_fault *vmf)
 		return VM_FAULT_SIGBUS;
 	addr += offset;
 
-	page = vmalloc_to_page(addr);
+	page = virt_to_page(addr);
 	get_page(page);
 	vmf->page = page;
 
@@ -220,7 +220,7 @@ static void hwdep_free(struct snd_hwdep *hwdep)
 {
 	struct snd_tscm *tscm = hwdep->private_data;
 
-	vfree(tscm->status);
+	kfree(tscm->status);
 	tscm->status = NULL;
 }
 
@@ -237,7 +237,7 @@ int snd_tscm_create_hwdep_device(struct snd_tscm *tscm)
 	struct snd_hwdep *hwdep;
 	int err;
 
-	tscm->status = vzalloc(sizeof(*tscm->status));
+	tscm->status = kzalloc(sizeof(*tscm->status), GFP_KERNEL);
 	if (!tscm->status)
 		return -ENOMEM;
 

commit 17774545465d548c4b29490c69227711b57b0bef

firewire-tascam: fix kernel oops to call vfree() to memory object allocated by kmalloc
Fixes: 231bd17e665b('firewire-tascam: fix to use memory object in GFP_KERNEL instead of vmalloc')

sound/firewire/tascam/tascam-hwdep.c
@@ -132,7 +132,7 @@ static int hwdep_release(struct snd_hwdep *hwdep, struct file *file)
 		tscm->dev_lock_count = 0;
 	spin_unlock_irq(&tscm->lock);
 
-	vfree(tscm->status);
+	kfree(tscm->status);
 
 	return 0;
 }
@@ -243,7 +243,7 @@ int snd_tscm_create_hwdep_device(struct snd_tscm *tscm)
 
 	err = snd_hwdep_new(tscm->card, "Tascam", 0, &hwdep);
 	if (err < 0) {
-		vfree(tscm->status);
+		kfree(tscm->status);
 		tscm->status = NULL;
 		return err;
 	}